1ae1450e09708a9f2892ffae15632601aedbaabc
min-zlee <almj1169@gmail.com>
2025-01-10 20:25:05 +0900

    🎉Character🎉

---
+++ Character.h
@@ -0,0 +1,29 @@
+﻿#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <string>
+
+class Character
+{
+public:
+    static Character* instance;
+    Character(const std::string& name);
+    static Character* getInstance(const std::string& name = "");
+
+    void displayStatus() const;
+    void levelUp();
+    void useItem(int index);
+
+private:
+    std::string name;
+    int level;
+    int health;
+    int maxHealth;
+    int attack;
+    int experience;
+    int gold;
+    //std::vector<Item*> inventory;아직구현x
+
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "Character.h"
 
 int main() {
-std::cout << "last"<< std::endl;
+    Character* myCharacter = Character::getInstance("치와와");
 
+    myCharacter->displayStatus(); // 초기 상태 출력
+
+    return 0;
 }
